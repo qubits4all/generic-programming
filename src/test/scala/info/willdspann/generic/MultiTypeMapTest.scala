80b424f68931220a73db5c74b45fa6e56b3af34e
Will D. Spann <qubits4all@users.noreply.github.com>
2020-06-14 02:31:04 -0600

    [test] Test application for MultiTypeMap
    - Created a simple test application for testing MultiTypeMap.

---
+++ src/test/scala/info/willdspann/generic/MultiTypeMapTest.scala
@@ -0,0 +1,107 @@
+package info.willdspann.generic
+
+import java.time.LocalDate
+
+object MultiTypeMapTest {
+
+    def main(args: Array[String]): Unit = {
+        val multiTypeMap = new MultiTypeMap[String]()
+
+        val eggs = "eggs"
+        multiTypeMap.put("spam", eggs)
+
+        val eggsReplaced = multiTypeMap.putIfAbsent("spam", "green eggs & ham")
+
+        multiTypeMap.putIfAbsent("parrot", "Pining for the fjords")
+
+        multiTypeMap.put("number", 42)
+        multiTypeMap.put("numbers", List(1, 1, 2, 3, 5, 8, 13))
+        multiTypeMap.put("pair", 3 -> 'i')
+
+        val marsBook = Book("Mars", "Ben Bova", LocalDate.of(1990, 6, 10), 532, eBook = true)
+        multiTypeMap.put("mars", marsBook)
+
+        println(s"Map size: ${multiTypeMap.size}")
+
+        println(s"Contains 'spam': ${multiTypeMap.contains("spam")}")
+        println(s"Value for 'spam' replaced? $eggsReplaced")
+
+        multiTypeMap.get("spam").map { eggs: String =>
+            println(s"spam: $eggs")
+        }.getOrElse {
+            println(s"spam: <null>")
+        }
+
+        val spam: String = multiTypeMap("spam")
+        println(s"'spam' via apply(): $spam")
+
+        println(s"Contains 'parrot': ${multiTypeMap.contains("parrot")}")
+
+        multiTypeMap.get("parrot").map { parrot: String =>
+            println(s"parrot: $parrot")
+        }.getOrElse {
+            println(s"parrot: <null>")
+        }
+
+        multiTypeMap.get("number").foreach { number: Int =>
+            println(s"number: $number")
+        }
+
+        multiTypeMap.get("numbers").foreach { numbers: List[Int] =>
+            println(s"numbers: $numbers")
+        }
+
+        println(s"Contains 'pair': ${multiTypeMap.contains("pair")}")
+        multiTypeMap.get[(Int, Char)]("pair").foreach {
+            case (i, c) => println(s"pair: $i -> $c")
+        }
+
+        val pair: (Int, Char) = multiTypeMap("pair")
+        println(s"'pair' via apply(): ${pair._1} -> ${pair._2}")
+
+        val (i: Int, c: Char) = multiTypeMap[(Int, Char)]("pair")
+        println(s"'pair' destructured via apply(): $i -> $c")
+
+        multiTypeMap.get("mars").foreach { book: Book =>
+            println(s"mars: $book")
+        }
+        val marsBookRetrieved: Book = multiTypeMap("mars")
+        println(s"'mars' via apply(): $marsBookRetrieved")
+
+        multiTypeMap.get[Int]("spam").map { foo =>
+            println(s"foo: $foo")
+        }.getOrElse {
+            println(s"No entry found with key: 'foo' and value type: Int")
+        }
+
+        multiTypeMap.get[Int]("foo").map { foo =>
+            println(s"foo: $foo")
+        }.getOrElse {
+            println(s"No entry found with key: 'foo' and value type: Int")
+        }
+
+        multiTypeMap.get[List[Int]]("foo").map { foo =>
+            println(s"foo: $foo")
+        }.getOrElse {
+            println(s"No entry found with key: 'foo' and value type: List[Int]")
+        }
+
+        println(s"Contains value 'eggs': ${multiTypeMap.containsValue("eggs")}")
+        println(s"Contains (literal) value 'eggs': ${multiTypeMap.containsLiteral["eggs"]}")
+
+        println(s"Contains value 'Pining for the fjords': ${multiTypeMap.containsValue("Pining for the fjords")}")
+
+        println(s"Contains value: 'foo': ${multiTypeMap.containsValue("foo")}")
+
+        println(s"Contains value: 42 : ${multiTypeMap.containsValue(42)}")
+        println(s"Contains (literal) value: 42 : ${multiTypeMap.containsLiteral[42]}")
+
+        println(s"Contains value: 3 -> 'i' : ${multiTypeMap.containsValue(3 -> 'i')}")
+
+        println(s"Contains value: $marsBook : ${multiTypeMap.containsValue(marsBook)}")
+
+        val moonwarBook = Book("Moonwar", "Ben Bova", LocalDate.of(1985, 3, 1), 740, eBook = false)
+        println(s"Contains value: $moonwarBook : ${multiTypeMap.containsValue(moonwarBook)}")
+    }
+
+}
